147db19db1e9f9733be6e74c7a5373e59fedd984
Jarvis <jarvis@knvsolutions.com>
2026-03-30 01:13:43 -0400

    Fix MainApplication.kt - match RN 0.74 template exactly

--- android/app/src/main/java/com/elevatedroots/MainApplication.kt
+++ android/app/src/main/java/com/elevatedroots/MainApplication.kt
@@ -3,11 +3,9 @@ package com.elevatedroots
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
@@ -17,18 +15,21 @@ class MainApplication : Application(), ReactApplication() {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {}
+
         override fun getJSMainModuleName(): String = "index"
+
         override fun getUseDeveloperSupport(): Boolean = false
+
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
-    load()
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      load()
+    }
   }
 }
