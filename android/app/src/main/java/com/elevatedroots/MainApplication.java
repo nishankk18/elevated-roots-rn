b75e08f5a950507b350431252b6a11345d284b8b
Jarvis <jarvis@knvsolutions.com>
2026-03-30 01:26:17 -0400

    Switch native classes to Java - fix constructor issues

---
+++ android/app/src/main/java/com/elevatedroots/MainApplication.java
@@ -0,0 +1,55 @@
+package com.elevatedroots;
+
+import android.app.Application;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.soloader.SoLoader;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost reactNativeHost = new DefaultReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return false;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return new PackageList(this).getPackages();
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+
+    @Override
+    public boolean isNewArchEnabled() {
+      return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+    }
+
+    @Override
+    public boolean isHermesEnabled() {
+      return BuildConfig.IS_HERMES_ENABLED;
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return reactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, false);
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      DefaultNewArchitectureEntryPoint.load();
+    }
+  }
+}
